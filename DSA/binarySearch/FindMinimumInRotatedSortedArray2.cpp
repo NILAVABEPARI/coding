608b20439a140fe492ff297b0e9c1e4ffd7968b6
NILAVABEPARI <nilavastnd@gmail.com>
2026-05-16 17:12:49 +0530

    added new method to solve findMinimumInRotatedSortedArray2 and changed the name of another file

--- DSA/binarySearch/FindMinimumInRotatedSortedArray2.cpp
+++ DSA/binarySearch/FindMinimumInRotatedSortedArray2.cpp
@@ -6,20 +6,42 @@ class Solution
 {
 public:
     // !! very important and need revision
+    // int findMin(vector<int> &nums)
+    // {
+    //     int low = 0, high = nums.size() - 1;
+    //     while (low <= high)
+    //     {
+    //         int mid = low + ((high - low) / 2);
+    //         if (nums[mid] < nums[high])
+    //             high = mid;
+    //         else if (nums[mid] > nums[high])
+    //             low = mid + 1;
+    //         else
+    //             high--;
+    //     }
+    //     return nums[low];
+    // }
+
+    // !! easier to understand code --
     int findMin(vector<int> &nums)
     {
-        int low = 0, high = nums.size() - 1;
+        int low = 0, high = nums.size() - 1, minEle = INT_MAX;
         while (low <= high)
         {
-            int mid = low + ((high - low) / 2);
-            if (nums[mid] < nums[high])
-                high = mid;
-            else if (nums[mid] > nums[high])
+            // remove the duplicates from both ends
+            while (low < high && nums[low] == nums[low + 1])
+                low++;
+            while (high > low && nums[high] == nums[high - 1])
+                high--;
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < minEle)
+                minEle = nums[mid];
+            if (nums[mid] > nums[high])
                 low = mid + 1;
             else
-                high--;
+                high = mid - 1;
         }
-        return nums[low];
+        return minEle;
     }
 };
 
