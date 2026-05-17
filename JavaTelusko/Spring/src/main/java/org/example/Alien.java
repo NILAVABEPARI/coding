aef4129d22033d0e29f726ea9a488488af299096
NILAVABEPARI <nilavastnd@gmail.com>
2026-05-17 15:32:21 +0530

    modified the spring project files

--- JavaTelusko/Spring/src/main/java/org/example/Alien.java
+++ JavaTelusko/Spring/src/main/java/org/example/Alien.java
@@ -1,7 +1,38 @@
 package org.example;
 
 public class Alien {
-    public void code(){
+    private int age;
+    private Laptop lap;
+
+    public Alien() {
+        System.out.println("Alien Constructor called");
+    }
+
+    public Alien(int age, Laptop lap) {
+        System.out.println("Alien parameterized Constructor called");
+        this.age = age;
+        this.lap = lap;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter called");
+        this.age = age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+    public void code() {
         System.out.println("Coding!!!");
+        lap.compile();
     }
 }
--- JavaTelusko/Spring/src/main/java/org/example/App.java
+++ JavaTelusko/Spring/src/main/java/org/example/App.java
@@ -7,8 +7,23 @@ public class App
 {
     public static void main( String[] args )
     {
+        // instead of writing Alien obj = new Alien(); spring is creating the object for me
+        // the below code starts a spring container
+        // all the objects mentioned in the spring.xml file will be created in this line
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        // the below line tells spring -- Give me the object whose id is alien
+        // we are type casting because by default .getBean returns an object type
+
+        // by default, we will get the reference of the same object because the scope is singleton
         Alien obj = (Alien) context.getBean("alien");
+
+        // we did not use the setter directly to set the age
+        System.out.println(obj.getAge());
         obj.code();
+
+        // Alien obj2 = (Alien) context.getBean("alien");
+        // obj2.code();
+
     }
 }
